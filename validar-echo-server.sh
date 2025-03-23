da6783664ab2a957b3aa6c89da2359972652cef7
TDPFIUBA <tdelpup@fi.uba.ar>
2025-03-23 00:07:31 -0300

    fix(ej3): update network name and add timeout for netcat in echo server validation script

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,13 +2,14 @@
 
 SERVER_NAME="server"
 SERVER_PORT=12345
-NETWORK_NAME="testing_net"
+NETWORK_NAME="tp0_testing_net"
 TEST_MESSAGE="Is this my echo?"
+NETCAT_TIMEOUT=2
 
 # Reuse busybox image (used in client Dockerfile)
 IMAGE="busybox"
 
-RESPONSE=$(docker run --rm --network $NETWORK_NAME $IMAGE sh -c "echo '$TEST_MESSAGE' | nc $SERVER_NAME $SERVER_PORT")
+RESPONSE=$(docker run --rm --network $NETWORK_NAME $IMAGE sh -c "echo '$TEST_MESSAGE' | nc -w $NETCAT_TIMEOUT $SERVER_NAME $SERVER_PORT 2>/dev/null")
 
 if [ "$RESPONSE" == "$TEST_MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
