8dde6e3834f73bf815e3d4108255e8659ce45397
TDPFIUBA <tdelpup@fi.uba.ar>
2025-03-23 00:23:54 -0300

    feat(ej4): implement graceful shutdown handling in server

--- server/common/server.py
+++ server/common/server.py
@@ -1,13 +1,46 @@
 import socket
 import logging
+import signal
 
 
 class Server:
     def __init__(self, port, listen_backlog):
-        # Initialize server socket
+        """Server initialization"""
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._server_socket.bind(('', port))
+        self._server_socket.bind(("", port))
         self._server_socket.listen(listen_backlog)
+        self._server_is_running = True
+
+        self._client_socket = None
+        self.__set_up_signal_handler()
+
+    def __release_socket(self, release_socket):
+        if release_socket:
+            try:
+                release_socket.shutdown(socket.SHUT_RDWR)
+                release_socket.close()
+            except Exception as e:
+                logging.error("action: socket_release | result: fail | error: {e}")
+        return None
+
+    def __set_up_signal_handler(self):
+        signal.signal(signal.SIGTERM, self.__handle_sigterm_signal)
+
+    def __handle_sigterm_signal(self):
+        """
+        Handle SIGTERM signal
+
+        When signal is received server stops accepting new connections and finishes the current
+        connection with the client before closing the server.
+        """
+        logging.info("action: sigterm_signal | result: in_progress")
+        try:
+            self._server_is_running = False
+            self._client_socket = self.__release_socket(self._client_socket)
+            self._server_socket = self.__release_socket(self._server_socket)
+            logging.info("action: sigterm_signal | result: success")
+        except Exception as e:
+            logging.error("action: sigterm_signal | result: fail | error: {e}")
 
     def run(self):
         """
@@ -18,13 +51,11 @@ class Server:
         finishes, servers starts to accept new connections again
         """
 
-        # TODO: Modify this program to handle signal to graceful shutdown
-        # the server
-        while True:
-            client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
+        while self._server_is_running:
+            self._client_socket = self.__accept_new_connection()
+            self.__handle_client_connection()
 
-    def __handle_client_connection(self, client_sock):
+    def __handle_client_connection(self):
         """
         Read message from a specific client socket and closes the socket
 
@@ -33,15 +64,18 @@ class Server:
         """
         try:
             # TODO: Modify the receive to avoid short-reads
-            msg = client_sock.recv(1024).rstrip().decode('utf-8')
-            addr = client_sock.getpeername()
-            logging.info(f'action: receive_message | result: success | ip: {addr[0]} | msg: {msg}')
+            msg = self._client_socket.recv(1024).rstrip().decode("utf-8")
+            addr = self._client_socket.getpeername()
+            logging.info(
+                f"action: receive_message | result: success | ip: {addr[0]} | msg: {msg}"
+            )
+
             # TODO: Modify the send to avoid short-writes
-            client_sock.send("{}\n".format(msg).encode('utf-8'))
+            self._client_socket.send("{}\n".format(msg).encode("utf-8"))
         except OSError as e:
             logging.error("action: receive_message | result: fail | error: {e}")
         finally:
-            client_sock.close()
+            self._client_socket = self.__release_socket(self._client_socket)
 
     def __accept_new_connection(self):
         """
@@ -51,8 +85,7 @@ class Server:
         Then connection created is printed and returned
         """
 
-        # Connection arrived
-        logging.info('action: accept_connections | result: in_progress')
+        logging.info("action: accept_connections | result: in_progress")
         c, addr = self._server_socket.accept()
-        logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
+        logging.info(f"action: accept_connections | result: success | ip: {addr[0]}")
         return c
